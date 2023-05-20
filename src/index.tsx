ac94f2057a7933e95516b8bcceb6f78eeea802d9
Ekuripusu <dogasshole.test@gmail.com>
2023-05-20 14:59:46 +0300

    add prettierrs.json

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,5 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(<React.StrictMode></React.StrictMode>);
